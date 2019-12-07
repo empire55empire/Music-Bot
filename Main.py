431244d6878a2cdbbf36688775b666d9dc256478
empire55empire <58105789+empire55empire@users.noreply.github.com>
2019-12-07 08:38:02 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,7 @@
+Our Music Bot Link& moduter bot -https://discordapp.com/api/oauth2/authorize?client_id=644502792374845461&permissions=8&scope=bot
+For music commend use this perfix is -<help 
+For Moduter Help commend $help
+More Funcation will come soon..........
+our server link -https://discord.gg/RuNgjzf
+If u Need Any help or want to purchase Any script of discord Plese join my Server & Dm me on this Devils{DEV}
+#9511 
